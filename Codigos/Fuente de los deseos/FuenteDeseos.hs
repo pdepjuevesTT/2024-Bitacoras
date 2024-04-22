0b983563fb8ec4360eb5577e382e44dbe0616e81
Melina Bugallo <melinabugallo@gmail.com>
2024-04-22 00:11:17 -0300

    Create FuenteDeseos.hs

---
+++ Codigos/Fuente de los deseos/FuenteDeseos.hs
@@ -0,0 +1,84 @@
+--------------------------------------- Dominio ---------------------------------------
+type Edad = Number
+type Nombre = String
+type Felicidonios = Number
+type Sueño = String
+
+
+data Persona = UnaPersona{
+    edad :: Edad,
+    nombre :: Nombre,
+    felicidonios :: Felicidonios,
+    sueños :: [Sueño]
+}
+
+------------------------------ Coeficiente de satisfaccion ------------------------------
+type Coeficiente = Number
+coeficienteDeSatisfaccion :: Persona -> Coeficiente
+coeficienteDeSatisfaccion persona 
+    |felicidonios persona > 50 && felicidonios persona <= 100 = moderadamenteFeliz persona
+    |felicidonios persona > 100 = muyFeliz persona
+    |otherwise = pocoFeliz
+
+muyFeliz :: Persona -> Coeficiente
+muyFeliz persona = felicidonios persona * edad persona
+
+moderadamenteFeliz :: Persona -> Coeficiente
+moderadamenteFeliz persona = cantidadDeSueños persona * felicidonios persona
+
+type Cantidad = Number
+cantidadDeSueños :: Persona -> Cantidad
+cantidadDeSueños persona = length (sueños persona)
+
+pocoFeliz :: Coeficiente
+pocoFeliz = 40
+
+------------------------------------- Nombre Largo -------------------------------------
+
+nombreLargo :: Persona -> Bool
+nombreLargo persona = longitudNombre persona > 10
+
+type Longitud = Number
+longitudNombre :: Persona -> Longitud
+longitudNombre persona = length (nombre persona)
+
+------------------------------------ Persona Suertuda ------------------------------------
+
+esSuertuda :: Persona -> Bool
+esSuertuda persona = even (3 * coeficienteDeSatisfaccion persona)
+
+--------------------------------------- Nombre Lindo --------------------------------------
+
+nombreLindo :: Persona -> Bool
+nombreLindo persona = primeraLetraNombre persona == 'L'
+
+type Letra = Char
+primeraLetraNombre :: Persona -> Letra
+primeraLetraNombre persona = head (nombre persona)
+
+-------------------------------------- Cumplir Sueños --------------------------------------
+
+cumplirSueños :: Persona -> Persona
+cumplirSueños persona = quedarseSinSueños (ganarFelicidonios persona)
+
+quedarseSinSueños :: Persona -> Persona
+quedarseSinSueños persona = persona {sueños = []}
+
+ganarFelicidonios :: Persona -> Persona
+ganarFelicidonios persona = persona { felicidonios = felicidonios persona + felicidoniosGanadosAlCumplirSueños persona}
+
+felicidoniosGanadosAlCumplirSueños :: Persona -> Felicidonios
+felicidoniosGanadosAlCumplirSueños persona = coeficienteDeSatisfaccion persona * cantidadDeSueños persona
+
+-------------------------------------- Fuente de los deseos --------------------------------------
+
+tirarMoneda :: Persona -> Persona
+tirarMoneda persona = ganar10Felicidonios (fuenteDeLosDeseos persona)
+
+fuenteDeLosDeseos :: Persona -> Persona
+fuenteDeLosDeseos persona
+    |esSuertuda persona = cumplirSueños persona
+    |otherwise = persona
+
+ganar10Felicidonios :: Persona -> Persona
+ganar10Felicidonios persona = persona {felicidonios = felicidonios persona + 10}
